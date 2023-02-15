30200fa1b4b17868b9ae3fa445b134192a931c55
Abhishek <abhishekydv98@outlook.com>
2023-02-15 21:38:29 +0530

    added zipkin in product and order and created new payment microservice

---
+++ PaymentService/src/main/java/com/paymentservice/service/PaymentService.java
@@ -0,0 +1,4 @@
+package com.paymentservice.service;
+
+public interface PaymentService {
+}
---
+++ PaymentService/src/main/java/com/paymentservice/service/PaymentServiceImpl.java
@@ -0,0 +1,7 @@
+package com.paymentservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentServiceImpl implements PaymentService{
+}
