27af19eb80e5ac3efe47aec0c71e59d569b78892
Abhishek <abhishekydv98@outlook.com>
2023-02-15 20:55:57 +0530

    added error exception handling in order for place order cases

---
+++ OrderService/src/main/java/com/orderservice/config/FeignConfig.java
@@ -0,0 +1,15 @@
+package com.orderservice.config;
+
+import com.orderservice.external.decoder.CustomErrorDecoder;
+import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    ErrorDecoder errorDecoder() {
+        return new CustomErrorDecoder();
+    }
+}
